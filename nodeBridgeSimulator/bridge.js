d885718bfc4ae01be61735007eee3f0ec21a0af6
Sven <sven@dev-vm.(none)>
2013-04-21 08:47:32 +0200

    added simulator

---
+++ nodeBridgeSimulator/bridge.js
@@ -0,0 +1,66 @@
+var express = require("express");
+var app = express();
+app.listen(80);
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
+};
+
+app.use(express.logger());
+app.use(express.static(__dirname + '/public_html'));
+app.use(express.bodyParser());
+app.use(allowCrossDomain);
+
+app.set('linkbuttonPushed', false);
+app.set('usernames', []);
+
+app.get('/linkbutton', function(request, response) {
+    app.set('linkbuttonPushed', true);
+    setTimeout(function() {
+        app.set('linkbuttonPushed', false);
+    }, 30000);
+    response.send(200, "link button pushed. you have 30 seconds to register new usernames");
+});
+
+app.get('/api/:username', function(request, response) {
+    response.send(200, [
+        {
+            error: {
+                type: 1,
+                address: '/',
+                description: 'unauthorized user'
+            }
+        }
+    ]);
+});
+
+app.post('/api', function(request, response) {
+    var devicetype = request.body.devicetype;
+    var username = request.body.username;
+    if (username === '') {
+        username = "letmegeneratethatforyou";
+    }
+    if (app.get('linkbuttonPushed')) {
+        app.get('usernames').push(username);
+        console.log(app.get('usernames'));
+        response.send(200, [
+            {
+                success: {
+                    username: username
+                }
+            }
+        ]);
+    } else {
+        response.send(200, [
+            {
+                error: {
+                    type: 101,
+                    address: '',
+                    description: 'link button not pressed'
+                }
+            }
+        ]);
+    }
+});
