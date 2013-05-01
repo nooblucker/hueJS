3edb4a92f818edd33f2744cd055486a6b27a5cf1
Sven <sven@dev-vm>
2013-05-01 12:50:40 +0200

    using bootstrap now. showing a list of useful commands in the debugger.

--- nodeBridgeSimulator/public_html/js/main.js
+++ nodeBridgeSimulator/public_html/js/main.js
@@ -1,4 +1,29 @@
 $(function() {
+
+    var logError = function(response) {
+        $("#log").prepend("<div class='error log'>" + response.status + " " + response.statusText + "</div>");
+    };
+
+    $("#linkbutton").on("click", function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: '/linkbutton'
+        }).done(function(response) {
+            $("#log").prepend("<div class='info log'>" + response + "</div>");
+        }).fail(logError);
+    });
+    
+    $("#commands button").on("click", function() {
+        var $this = $(this);
+        var body = $this.attr("data-body");
+        var url = $this.attr("data-url");
+        var method = $this.attr("data-method");
+        
+        $("input[name='req-method'][value='"+method+"']").prop("checked", true);
+        $("#req-url").val(url);
+        $("#req-body").val(body);
+    });
+
     $("#request").on("submit", function(event) {
         event.preventDefault();
 
@@ -7,29 +32,29 @@ $(function() {
         var body = $("#req-body").val();
         var data;
         try {
-            if (body !== "")
-            data = $.parseJSON(body);
+            if (body !== "") {
+                data = JSON.parse(body);
+                body = "<pre>"+body+"</pre>";
+            }
         } catch (e) {
             alert('malformed json: ' + e);
             return;
         }
-        
-        $("#log").prepend("<div class='request'>"+method+" "+url+" <pre>"+body+"</pre></div>");
-        
+
+        $("#log").prepend("<div class='request log'>" + method + " " + url + " " + body + "</div>");
+
         var req = $.ajax({
             url: url,
             method: method,
             data: data,
             dataType: "json"
         });
-        
+
         req.done(function(response) {
-            $("#log").prepend("<div class='response'><pre>"+response+"</pre></div>");
+            $("#log").prepend("<div class='response log'><pre>" + JSON.stringify(response) + "</pre></div>");
         });
-        
-        req.fail(function(response) {
-            alert(response.status + " " + response.statusText);
-        });
-        
+
+        req.fail(logError);
+
     });
 });
